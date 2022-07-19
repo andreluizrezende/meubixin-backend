4e7db0a5e352badfe997bd70e8688a81c77c3a18
Lucas Góes <74834174+lucas641@users.noreply.github.com>
2022-07-19 19:35:18 -0300

    s3

--- routes/uploadImagens/upload.js
+++ routes/uploadImagens/upload.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const upload = require('../../utils/multer');
 const fs = require('fs');
+const { getFileStream, listBuckets, createBucket, uploadFile, listFileStream, deleteFile } = require('../../utils/s3');
 const path = require('path');
 const models = require('../../models');
 const { mob_imagens_feridas } = models;
@@ -9,22 +10,31 @@ const { mob_imagens_feridas } = models;
 route.get('/upload/:key', async (req, res) => {
   try {
     const { key } = req.params;
-    fs.readFile(path.resolve(__dirname, "..", "..", "uploads", `${key}`), (err, data) => {
-      if (err) return res.send(err);
-      res.send(data);
-    });
+    const keyS3 = key.split('.')[0];
+    const readStream = await getFileStream(keyS3);
+    if (!readStream) return res.send(false);
+    readStream.pipe(res);
   } catch (error) {
     console.log('ERRO em /upload');
     console.log(error.message);
   }
 });
 
+route.get('/upload', (req, res) => {
+  return res.send(listFileStream());
+});
+
 route.post('/upload', upload.single("img"), async (req, res) => {
   try {
     const id = Number(req.body.id);
+    const fileStream = fs.createReadStream(req.file.path);
     const file = req.file.filename;
+
     const ds_caminho_server = `http://localhost:3000/upload/${file}`;
     const resposta = await mob_imagens_feridas.update({ ds_caminho_server }, { where: { id } });
+
+    uploadFile(fileStream, file);
+
     resposta[0] ? res.send(ds_caminho_server) : res.send(false);
   } catch (error) {
     console.log('ERRO em /upload');
@@ -35,15 +45,14 @@ route.post('/upload', upload.single("img"), async (req, res) => {
 route.put('/upload', upload.single("img"), async (req, res) => {
   try {
     const { key } = req.body;
+    const keyS3 = key.split('.')[0];
     const id = Number(req.body.id);
-    const pathFile = path.resolve(__dirname, "..", "..", "uploads", `${key}`);
+    const fileStream = fs.createReadStream(req.file.path);
     const file = req.file.filename;
+
+
     const ds_caminho_server = `http://localhost:3000/upload/${file}`;
-
-    fs.rm(path.resolve(pathFile), (err, data) => {
-      if (err) return res.send(err);
-    });
-
+    uploadFile(fileStream, keyS3);
     const resposta = await mob_imagens_feridas.update({ ds_caminho_server }, { where: { id } });
 
     resposta[0] ? res.send(ds_caminho_server) : res.send(false);
@@ -56,7 +65,9 @@ route.put('/upload', upload.single("img"), async (req, res) => {
 route.delete('/upload/:key', async (req, res) => {
   try {
     const { key } = req.params;
+    const keyS3 = key.split('.')[0];
     const pathFile = path.resolve(__dirname, "..", "..", "uploads", `${key}`);
+    deleteFile(keyS3);
     fs.rm(path.resolve(pathFile), (err, data) => {
       if (err) return res.send(err);
       res.send(true);
@@ -65,6 +76,15 @@ route.delete('/upload/:key', async (req, res) => {
     console.log('ERRO em /upload');
     console.log(error.message);
   }
+});
+
+route.get('/uploadListeningBucket', async (req, res) => {
+  return res.send(listBuckets());
+});
+
+route.post('/uploadCreateBucket', async (req, res) => {
+  const { name } = req.body;
+  return res.send(createBucket(name));
 });
 
 module.exports = route;
---
+++ utils/s3.js
@@ -0,0 +1,110 @@
+require('dotenv').config();
+const { S3Client, PutObjectCommand, ListBucketsCommand, CreateBucketCommand, GetObjectCommand, ListObjectsCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+
+const Bucket = AWS_BUCKET_NAME = "cicatribioskin";
+const region = AWS_BUCKET_REGION = "us-east-2";
+const accessKeyId = AWS_ACCESS_KEY = "REMOVED_AWS_ACCESS_KEY";
+const secretAccessKey = AWS_SECRET_KEY = "REMOVED_AWS_SECRET_KEY";
+
+const s3 = new S3Client({
+  region,
+  credentials: { accessKeyId, secretAccessKey }
+});
+
+// uploads a file to s3
+async function uploadFile(Body, Key) {
+  const uploadParams = { Bucket, Body, Key }
+
+  try {
+    const data = await s3.send(new PutObjectCommand(uploadParams));
+    console.log(
+      "Successfully uploaded object: " +
+      uploadParams.Bucket + "/" + uploadParams.Key
+    );
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+// downloads a file from s3
+async function getFileStream(Key) {
+  const downloadParams = { Key, Bucket }
+
+  try {
+    const streamToString = (stream) =>
+      new Promise((resolve, reject) => {
+        const chunks = [];
+        stream.on("data", (chunk) => chunks.push(chunk));
+        stream.on("error", reject);
+        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+      });
+
+    const data = await s3.send(new GetObjectCommand(downloadParams));
+
+    const bodyContents = await streamToString(data.Body);
+    console.log(bodyContents);
+    return bodyContents;
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function listFileStream() {
+  try {
+    const bucketParams = { Bucket }
+    const data = await s3.send(new ListObjectsCommand(bucketParams));
+    console.log("Success", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+// delete file from s3
+async function deleteFile(Key) {
+  let params = {
+    Bucket,
+    Key
+  }
+  try {
+    const data = await s3.send(new DeleteObjectCommand(params));
+    console.log(
+      "Successfully delete object: " +
+      params.Bucket + "/" + params.Key
+    );
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+const listBuckets = async () => {
+  try {
+    const data = await s3.send(new ListBucketsCommand({}));
+    console.log("Success", data.Buckets);
+    if (data) return data.Buckets; else return false;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+const createBucket = async name => {
+  const bucketParams = { Bucket: name }
+  try {
+    const data = await s3.send(new CreateBucketCommand(bucketParams));
+    console.log("Success", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+module.exports = {
+  deleteFile,
+  getFileStream,
+  uploadFile,
+  listBuckets,
+  createBucket,
+  listFileStream
+}
