aeb57f4aac7f1b38d271bc1a10569c84096dc766
beatriiss <isisbeatrizsp2010@hotmail.com>
2023-02-10 11:34:53 -0300

    Consulta parar buscar apenas animais de um unico tutor

--- routes/tutorAnimal/animal.js
+++ routes/tutorAnimal/animal.js
@@ -45,7 +45,7 @@ route.get('/animalNome/:no_nome/:id_tutor', async (req, res) => {
     const { no_nome, id_tutor } = req.params;
     console.log(no_nome, id_tutor)
     const resposta = await mob_animais.findAll({
-     where: { no_nome: { [Op.like]: `${no_nome}%`, mob_tutores_id: id_tutor } }
+     where: { no_nome: { [Op.like]: `${no_nome}%`}, mob_tutores_id: id_tutor  }
     });
     resposta ? res.send(resposta) : res.send(false);
   } catch (error) {
