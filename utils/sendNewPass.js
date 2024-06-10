ce405a0550d508288713bc12862cccae37ec6392
Beatriz <71399250+beatriiss@users.noreply.github.com>
2024-06-10 18:37:35 -0300

    Update sendNewPass.js

--- utils/sendNewPass.js
+++ utils/sendNewPass.js
@@ -1,31 +1,49 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-function sendEmail(ds_email, ds_senha){
+function sendEmail(ds_email, ds_senha) {
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'suportecicatribio@gmail.com',
-          pass: 'dqqujdtqiyermamv'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'isisbeatris.dev@gmail.com',
-        to: ds_email,
-        subject: 'Recuperação da sua senha de acesso',
-        text: `Essa é sua nova senha de acesso ${ds_senha}`
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } 
-      });
+  let transporter = nodemailer.createTransport({
+    host: "smtp.hostinger.com", 
+    port: 587, 
+    secure: false, 
+    auth: {
+      user: "suporte@cicatribio.com.br", 
+      pass: "$up@Rt3ApP", 
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
+ 
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log("Erro na configuração do transportador: ", error);
+    } else {
+      console.log("Transportador de e-mail pronto: ", success);
+    }
+  });
+
+
+  let mailOptions = {
+    from: "suporte@cicatribio.com.br", 
+    to: ds_email, 
+    subject: "Recuperação de senha: Aplicativo CicatribioVET", 
+    text: "Essa é a sua nova senha: " + ds_senha, 
+  };
+
+  
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log("Erro ao enviar e-mail: ", error);
+    }
+    console.log("E-mail enviado: %s", info.messageId);
+    console.log("Informações do e-mail: ", info);
+  });
 }
 
 module.exports = {
-sendEmail
-  }
+  sendEmail,
+};
+
 
