58e41fea05d5a828f9c4c75c380080da65230e45
beatrisiis <isisbeatrizsp2010@hotmail.com>
2025-03-28 00:03:59 -0300

    add mob_parceiros_id

--- models/mob_animais.js
+++ models/mob_animais.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   Mob_animais.init({
     mob_tutores_id: DataTypes.INTEGER,
     mob_veterinarios_id: DataTypes.INTEGER,
+    mob_parceiros_id: DataTypes.INTEGER,
     no_nome: DataTypes.STRING,
     ds_especie: DataTypes.STRING,
     mob_especies_id: DataTypes.INTEGER,
