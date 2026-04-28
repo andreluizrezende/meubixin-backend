d343b0c161c6a46157298a8213017d457ddd915b
beatrisiis <isisbeatrizsp2010@hotmail.com>
2026-04-28 06:40:30 -0300

    alerando tamanho do banco

--- models/web_anamneses.js
+++ models/web_anamneses.js
@@ -37,7 +37,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: true
       },
       ds_quadro_clinico: {
-        type: DataTypes.STRING(255),
+        type: DataTypes.STRING(2055),
         allowNull: true
       },
       ds_resultados_exames_anteriores: {
